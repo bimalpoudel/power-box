36fe4e6acbc03d239ccfd7ad71a1e37c934bf2d3
Bimal Poudel <gist@bimal.org.np>
2016-04-30 01:34:42 -0600

    Formatting.

--- power-box/power-box.php
+++ power-box/power-box.php
@@ -20,7 +20,7 @@ class power_box extends WP_Widget
 	private $attribution_url = 'http://bimal.org.np/';
 
 	public function __construct() {
-		parent::__construct(false, 'Power Box', array('description' => 'Displays almost any tiny contents.'));
+		parent::__construct(false, 'Power Box', array('description' => 'Displays almost any contents.'));
 		add_action('widgets_init', array($this, 'register'));
 	}
 
@@ -32,42 +32,42 @@ class power_box extends WP_Widget
 		$instance['attribution_url'] = !empty($instance['attribution_url'])?esc_attr($instance['attribution_url']):'';
 		?>
 		
-		<h4>Please fill up the all fields below.</h4>
+<h4>Please fill up the all fields below.</h4>
 
-		<p>
-		<label for="<?php echo $this->get_field_id('power_title'); ?>">Box's Heading:</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('power_title'); ?>" name="<?php echo $this->get_field_name('power_title'); ?>" type="text" value="<?php echo $instance['power_title']; ?>" placeholder="<?php echo $this->power_title; ?>" />
-		</p>
+<p>
+	<label for="<?php echo $this->get_field_id('power_title'); ?>">Box's Heading:</label>
+	<input class="widefat" id="<?php echo $this->get_field_id('power_title'); ?>" name="<?php echo $this->get_field_name('power_title'); ?>" type="text" value="<?php echo $instance['power_title']; ?>" placeholder="<?php echo $this->power_title; ?>" />
+</p>
 
-		<p>
-		<label for="<?php echo $this->get_field_id('power_url'); ?>">Content URL (Main Source):</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('power_url'); ?>" name="<?php echo $this->get_field_name('power_url'); ?>" type="text" value="<?php echo $instance['power_url']; ?>" placeholder="<?php echo $this->power_url; ?>" />
-		</p>
+<p>
+	<label for="<?php echo $this->get_field_id('power_url'); ?>">Content URL (Main Source):</label>
+	<input class="widefat" id="<?php echo $this->get_field_id('power_url'); ?>" name="<?php echo $this->get_field_name('power_url'); ?>" type="text" value="<?php echo $instance['power_url']; ?>" placeholder="<?php echo $this->power_url; ?>" />
+</p>
 
-		<p>
-		<label for="<?php echo $this->get_field_id('attribution_name'); ?>">Attribution Name (optional):</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('attribution_name'); ?>" name="<?php echo $this->get_field_name('attribution_name'); ?>" type="text" value="<?php echo $instance['attribution_name']; ?>" placeholder="<?php echo $this->attribution_name; ?>" />
-		</p>
-		
-		<p>
-		<label for="<?php echo $this->get_field_id('attribution_url'); ?>">Attribution URL (optional):</label>
-		<input class="widefat" id="<?php echo $this->get_field_id('attribution_url'); ?>" name="<?php echo $this->get_field_name('attribution_url'); ?>" type="text" value="<?php echo $instance['attribution_url']; ?>" placeholder="<?php echo $this->attribution_url; ?>" />
-		</p>
+<p>
+	<label for="<?php echo $this->get_field_id('attribution_name'); ?>">Attribution Name (optional):</label>
+	<input class="widefat" id="<?php echo $this->get_field_id('attribution_name'); ?>" name="<?php echo $this->get_field_name('attribution_name'); ?>" type="text" value="<?php echo $instance['attribution_name']; ?>" placeholder="<?php echo $this->attribution_name; ?>" />
+</p>
 
-		<h4>Help</h4>
-		<ul>
-			<li><strong>Box's Heading</strong>: Widget Box's Title</li>
-			<li><strong>Content URL</strong>: The actual remote tiny content, <a href="http://bimal.org.np/micro-services/one-liners.php">copy example</a></li>
-			<li><strong>Attribution Name</strong>: Link back text</li>
-			<li><strong>Attribution URL</strong>: URL to go there</li>
-		</ul>
+<p>
+	<label for="<?php echo $this->get_field_id('attribution_url'); ?>">Attribution URL (optional):</label>
+	<input class="widefat" id="<?php echo $this->get_field_id('attribution_url'); ?>" name="<?php echo $this->get_field_name('attribution_url'); ?>" type="text" value="<?php echo $instance['attribution_url']; ?>" placeholder="<?php echo $this->attribution_url; ?>" />
+</p>
+
+<h4>Help</h4>
+<ul>
+	<li><strong>Box's Heading</strong>: Widget Box's Title</li>
+	<li><strong>Content URL</strong>: The actual remote tiny content, <a href="http://bimal.org.np/micro-services/one-liners.php">copy example</a></li>
+	<li><strong>Attribution Name</strong>: Link back text</li>
+	<li><strong>Attribution URL</strong>: URL to go there</li>
+</ul>
 		<?php
 	}
 
 	public function update($new_instance, $old_instance) {
 		$instance = $old_instance;
 		$instance['power_title'] = strip_tags($new_instance['power_title']);
-		$instance['power_url'] = strip_tags($new_instance['power_url']);
+		$instance['power_url'] = filter_var(strip_tags($new_instance['power_url']), FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
 		$instance['attribution_name'] = strip_tags($new_instance['attribution_name']);
 		$instance['attribution_url'] = strip_tags($new_instance['attribution_url']);
 		return $instance;
@@ -76,6 +76,7 @@ class power_box extends WP_Widget
 	public function widget($args, $instance)
 	{
 		if(empty($instance['power_url'])) return false;
+		$instance['power_url'] = filter_var($instance['power_url'], FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
 
 		#print_r(func_get_args());
 		echo $args['before_widget'];
@@ -84,7 +85,8 @@ class power_box extends WP_Widget
 			echo "<h3>{$instance['power_title']}</h3>";
 			
 			/**
-			 * The actual remote content; should be valid to process
+			 * The actual remote content; should be valid URL to process
+			 * @see https://vip.wordpress.com/documentation/best-practices/fetching-remote-data/
 			 */
 			$quote = wp_remote_fopen($instance['power_url']);
 			echo $quote;
