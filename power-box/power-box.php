b4566fbf5d7ad6bae488301e8aad559a5118477e
Bimal Poudel <gist@bimal.org.np>
2016-05-03 15:11:45 -0600

    Release ready.

--- power-box/power-box.php
+++ power-box/power-box.php
@@ -14,9 +14,9 @@ class power_box extends WP_Widget
 	/**
 	 * Each of these placeholder hinting values should be setup within Widget Interface
 	 */
-	private $power_title = 'One Liners';
-	private $power_url = 'http://bimal.org.np/micro-services/one-liners.php';
-	private $attribution_name = 'bimal.org.np';
+	private $title = 'Power Box Works';
+	private $power_url = 'http://bimal.org.np/micro-services/example.php';
+	private $attribution_name = 'By - Bimal Poudel';
 	private $attribution_url = 'http://bimal.org.np/';
 
 	public function __construct() {
@@ -24,31 +24,30 @@ class power_box extends WP_Widget
 		add_action('widgets_init', array($this, 'register'));
 	}
 
+	public function register()
+	{
+		register_widget(__CLASS__);
+	}
+
 	public function form($instance)
 	{
-		$instance['power_title'] = !empty($instance['power_title'])?esc_attr($instance['power_title']):'';
+		$instance['title'] = !empty($instance['title'])?esc_attr($instance['title']):'';
 		$instance['power_url'] = !empty($instance['power_url'])?esc_attr($instance['power_url']):'';
 		$instance['attribution_name'] = !empty($instance['attribution_name'])?esc_attr($instance['attribution_name']):'';
 		$instance['attribution_url'] = !empty($instance['attribution_url'])?esc_attr($instance['attribution_url']):'';
 		?>
-		
-<h4>Please fill up the all fields below.</h4>
-
 <p>
-	<label for="<?php echo $this->get_field_id('power_title'); ?>">Box's Heading:</label>
-	<input class="widefat" id="<?php echo $this->get_field_id('power_title'); ?>" name="<?php echo $this->get_field_name('power_title'); ?>" type="text" value="<?php echo $instance['power_title']; ?>" placeholder="<?php echo $this->power_title; ?>" />
+	<label for="<?php echo $this->get_field_id('title'); ?>">Content Heading *:</label>
+	<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $instance['title']; ?>" placeholder="<?php echo $this->title; ?>" />
 </p>
-
 <p>
-	<label for="<?php echo $this->get_field_id('power_url'); ?>">Content URL (Main Source):</label>
+	<label for="<?php echo $this->get_field_id('power_url'); ?>">Micro Content URL *:</label>
 	<input class="widefat" id="<?php echo $this->get_field_id('power_url'); ?>" name="<?php echo $this->get_field_name('power_url'); ?>" type="text" value="<?php echo $instance['power_url']; ?>" placeholder="<?php echo $this->power_url; ?>" />
 </p>
-
 <p>
 	<label for="<?php echo $this->get_field_id('attribution_name'); ?>">Attribution Name (optional):</label>
 	<input class="widefat" id="<?php echo $this->get_field_id('attribution_name'); ?>" name="<?php echo $this->get_field_name('attribution_name'); ?>" type="text" value="<?php echo $instance['attribution_name']; ?>" placeholder="<?php echo $this->attribution_name; ?>" />
 </p>
-
 <p>
 	<label for="<?php echo $this->get_field_id('attribution_url'); ?>">Attribution URL (optional):</label>
 	<input class="widefat" id="<?php echo $this->get_field_id('attribution_url'); ?>" name="<?php echo $this->get_field_name('attribution_url'); ?>" type="text" value="<?php echo $instance['attribution_url']; ?>" placeholder="<?php echo $this->attribution_url; ?>" />
@@ -57,7 +56,10 @@ class power_box extends WP_Widget
 <h4>Help</h4>
 <ul>
 	<li><strong>Box's Heading</strong>: Widget Box's Title</li>
-	<li><strong>Content URL</strong>: The actual remote tiny content, <a href="http://bimal.org.np/micro-services/one-liners.php">copy example</a></li>
+	<li><strong>Content URL</strong>: The actual remote tiny content<br>
+		<a href="http://bimal.org.np/micro-services/one-liners.php" target="example">copy example</a> | 
+		<a href="http://github.com/bimalpoudel/micro-services/" target="documentation">Documentation</a>
+	</li>
 	<li><strong>Attribution Name</strong>: Link back text</li>
 	<li><strong>Attribution URL</strong>: URL to go there</li>
 </ul>
@@ -66,7 +68,7 @@ class power_box extends WP_Widget
 
 	public function update($new_instance, $old_instance) {
 		$instance = $old_instance;
-		$instance['power_title'] = strip_tags($new_instance['power_title']);
+		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['power_url'] = filter_var(strip_tags($new_instance['power_url']), FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED);
 		$instance['attribution_name'] = strip_tags($new_instance['attribution_name']);
 		$instance['attribution_url'] = strip_tags($new_instance['attribution_url']);
@@ -81,15 +83,15 @@ class power_box extends WP_Widget
 		#print_r(func_get_args());
 		echo $args['before_widget'];
 		echo '<div class="widget-text power_box">';
-			#$power_title = apply_filters('widget_power_title', $instance['power_title']);
-			echo "<h3>{$instance['power_title']}</h3>";
+			#$title = apply_filters('widget_title', $instance['title']);
+			echo "<h3>{$instance['title']}</h3>";
 			
 			/**
 			 * The actual remote content; should be valid URL to process
 			 * @see https://vip.wordpress.com/documentation/best-practices/fetching-remote-data/
 			 */
-			$quote = wp_remote_fopen($instance['power_url']);
-			echo $quote;
+			$remote_content = wp_remote_fopen($instance['power_url']);
+			echo "<div class='power_box_content'>{$remote_content}</div>";;
 			
 			if($instance['attribution_name'] && $instance['attribution_url'])
 			{
@@ -97,12 +99,6 @@ class power_box extends WP_Widget
 			}
 		echo '</div>';
 		echo $args['after_widget'];
-
-	}
-	
-	public function register()
-	{
-		register_widget(__CLASS__);
 	}
 }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,33 @@
 # Power Box
 
-A WordPress Widget with ambitions to power the sidebar with user configured contents.
+A WordPress Widget with ambitions to power the sidebar with user configured contents. Normally, micro-services.
+
+
+## Fields
+
+### Title
+ - Few words about the content box
+
+
+### URL
+
+ * Which URL to see for contents?
+ * Prefer to have a micro-service like
+   - Tip of the Day
+   - Picture of the Day
+
+
+### Attribution
+
+Optionally, give credits to your content source by link backs.
+
+ * Name
+ * URL
+
+
+## Usage Examples
+
+
+### Twitter Feeds
+
+How to put a Twitter Feed
